543b4df86c0bc1ddf9a69a2222417cd81f32e665
Dmytro POGREBNIAK <dpogrebn@e1r11p2.unit.ua>
2018-02-19 19:02:33 +0200

    lol

--- libft/ft_addletter.c
+++ libft/ft_addletter.c
@@ -6,7 +6,8 @@ char	*ft_addletter(char *string, char letter)
 	char	*str_copy;
 
 	str_copy = string;
-	str = (char *)malloc(sizeof(char) + 1);
+	if (!(str = (char *)malloc(sizeof(char) + 1)))
+		return (0);
 	str[0] = letter;
 	str[1] = '\0';
 	string = ft_strjoin(str_copy, str);
